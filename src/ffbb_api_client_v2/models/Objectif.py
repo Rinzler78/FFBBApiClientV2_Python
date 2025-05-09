091abc1e547fe968285b7e0f1edaf3b388c4e870
Boris LECLERE <Borisleclere@gmail.com>
2025-05-09 17:12:32 +0200

    Remove unused modules

--- src/ffbb_api_client_v2/models/Objectif.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-
-class Objectif(Enum):
-    ACCOMPAGNEMENT = "Accompagnement"
-    CURATIF = "Curatif"
-    PRÉVENTIF = "Préventif"
--- src/ffbb_api_client_v2/models/TypeEnum.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-
-class TypeEnum(Enum):
-    COMITÉ = "Comité"
-    FÉDÉRATION = "Fédération"
-    LIGUE = "Ligue"
